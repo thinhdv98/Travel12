010db42623aac412ea68f5dcdb50a2890794e504
Your Name <dothinh20598@gmail.com>
2019-03-25 23:09:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/travel/activity_Place.java
@@ -0,0 +1,13 @@
+package com.example.travel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_Place extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__place);
+    }
+}
